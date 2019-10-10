4d755d3c77e04bf89cde349a1c61ef823cf8ebf5
nhaucorp <nazmul@springrainit.com>
2019-10-10 14:38:20 +0600

    text input value read and console in btn press

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    "Open up App.js to start working on your app!"
-  );
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Course goal" style={{borderColor:'black',borderWidth:1,padding:10}}/>
-      <Button
-        onPress={() => setOutputText("Text changed!")}
-        title="Add"
+      <TextInput
+        value={enteredGoal}
+        placeholder="Course goal"
+        style={{ borderColor: "black", borderWidth: 1, padding: 10, flex: 1 }}
+        onChangeText={goalInputHandler}
       />
+      <Button onPress={addGoalHandler} title="Add" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection:"row",
-    padding:30
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around"
   }
 });
