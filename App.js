6fed13bb807044e90bfe179cdd3ef8eb118695a6
nhaucorp <nazmul@springrainit.com>
2019-10-10 15:42:31 +0600

    scrollview added

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput, Text, ScrollView } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
   };
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
   };
   return (
     <View style={styles.container}>
-      <TextInput
-        value={enteredGoal}
-        placeholder="Course goal"
-        style={{ borderColor: "black", borderWidth: 1, padding: 10, flex: 1 }}
-        onChangeText={goalInputHandler}
-      />
-      <Button onPress={addGoalHandler} title="Add" />
+      <View style={{ flexDirection: "row", justifyContent: "space-around", alignItems: "center" }}>
+        <TextInput
+          value={enteredGoal}
+          placeholder="Course goal"
+          style={{ borderColor: "black", borderWidth: 1, padding: 10, width: 200 }}
+          onChangeText={goalInputHandler}
+        />
+        <Button onPress={addGoalHandler} title="Add" />
+      </View>
+      <ScrollView>
+        {courseGoals.map((goal, index) => (
+          <View
+            key={index.toString()}
+            style={{
+              padding: 10,
+              backgroundColor: "#ccc",
+              borderColor: "black",
+              borderWidth: 1,
+              marginVertical: 10
+            }}
+          >
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -26,7 +45,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     marginTop: 30,
-    flexDirection: "row",
     justifyContent: "space-around"
   }
 });
